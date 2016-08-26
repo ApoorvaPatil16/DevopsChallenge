767798555a50da5b80bf4a19bca357c318a03bdd
unknown <vishal221092@gmail.com>
2016-08-26 20:16:28 +0530

    changes in data model listing v2

--- webapp/listdomain/controllers/listdomaincontroller.js
+++ webapp/listdomain/controllers/listdomaincontroller.js
@@ -3,32 +3,21 @@ angular.module('datamill')
     $scope.count = 0;
     $scope.searchAllDomain = function() {
       listDomainFactory.getDomainItems($scope.count).then(function(res) {
+        console.log($scope.count);
         $scope.domainItems = res;
         $scope.length = res.length;
       });
     }
     $scope.searchAllDomain();
-    $scope.searchDomainFunction = function() {
-      listDomainFactory.getDomainItems().then(function(res) {
-        $scope.domainItems = res;
-        $scope.length = res.length;
-      });
-    }
+    $scope.searchDomainFunction = function() {}
     $scope.searchNextFunction = function() {
-      if ($scope.length == 0) {
-        $scope.errorMessage = "No more records available";
-      } else {
-        $scope.count = $scope.count + 5;
-        $scope.searchAllDomain();
-      }
+
+      $scope.count = $scope.count + 10;
+      $scope.searchAllDomain();
 
     }
     $scope.searchPreviousFunction = function() {
-      if ($scope.count == 0) {
-        $scope.errorMessage = "No more previous records available";
-      } else {
-        $scope.count = $scope.count - 5;
-        $scope.searchAllDomain();
-      }
+      $scope.count = $scope.count - 10;
+      $scope.searchAllDomain();
     }
   }]);
