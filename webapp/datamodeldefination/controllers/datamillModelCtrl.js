fc2d252815e63a6d22b1f787549ea302a977998e
Prashant Farkya <prashant.farkya@gmail.com>
2016-08-26 14:15:24 +0530

    fixing the layout for datamillModel

--- webapp/datamodeldefination/controllers/datamillModelCtrl.js
+++ webapp/datamodeldefination/controllers/datamillModelCtrl.js
@@ -13,32 +13,16 @@ angular.module('datamill')
   });
 
 function datamillModelController($scope, datamodeldefinationservice, $element, $transclude, $log) {
-  var ctrl = this;
-  $scope.iseditable = -1;
+  var ctrl = this
   datamodeldefinationservice.getDomain().then(function(res) {    ctrl.domain = res;    }, function(res) { $log.info("failed to get"); });
   datamodeldefinationservice.getAttrType().then(function(res) {    ctrl.attrtype = res;    }, function(res) { $log.info("failed to get"); });
-  $scope.add = function() {
+  ctrl.add = function() {
       ctrl.attrs.push({});
     }
-    /*it will remove the one form */
+    /*it will remove the one attribute from datamilldomain */
   ctrl.remove = function(index) {
-      ctrl.attrs.splice(index, 1);
-    }
-    /*it will save and update on parent the one form */
-  ctrl.save = function(index, attribute) {
-      console.log($scope.attrs);
-      angular.copy(attribute, ctrl.attrs[index]);
-      ctrl.iseditable = -1;
-    }
-    /*it will cancel editing on parent the one form */
-  ctrl.cancel = function(index) {
-      ctrl.iseditable = -1;
-    }
-    /*it will enable editing on parent the one form */
-  ctrl.edit = function(index) {
-    ctrl.iseditable = index;
+    ctrl.attrs.splice(index, 1);
   }
-  ctrl.isopen = false;
   $log.info(ctrl.formTitle)
 
 }
