cf4481b23a233fc667eb76483e5765d194834c36
unknown <vishal221092@gmail.com>
2016-08-29 12:11:36 +0530

    ip changes

--- webapp/authservice/authFactory.js
+++ webapp/authservice/authFactory.js
@@ -2,14 +2,14 @@ angular.module('datamill')
   .factory('authFactory', function($http) {
     return {
       getNavbarItems: function() {
-        return $http.get("http://localhost:7070/navbarItems").then(function(res) {
+        return $http.get("http://172.23.238.188:7070/navbarItems").then(function(res) {
           return res.data;
         })
       },
       postNewDomain: function(postdata) {
         return $http({
           method: "POST",
-          url: "http://localhost:7070/domain",
+          url: "http://172.23.238.188:7070/domain",
           data: postdata,
           'content-type': "aplication/json"
         }).then(function(res) {
--- webapp/datamodeldefination/services/datamodeldefinationservice.js
+++ webapp/datamodeldefination/services/datamodeldefinationservice.js
@@ -2,19 +2,19 @@ angular.module('datamill')
   .factory('datamodeldefinationservice', function($http) {
     return {
       getDomain: function() {
-        return $http.get("http://localhost:7070/domain").then(function(res) {            
+        return $http.get("http://172.23.238.188:7070/domain").then(function(res) {            
           return res.data;        
         }, function(res) {
           return "Unable to connect server";
         })
       },
       getAttrType: function() {
-        return $http.get("http://localhost:7070/attrtype").then(function(res) {            
+        return $http.get("http://172.23.238.188:7070/attrtype").then(function(res) {            
           return res.data;        
         })
       },
       getDataModelConfig: function() {
-        return $http.get("http://localhost:7070/datamodelconf").then(function(res) {
+        return $http.get("http://172.23.238.188:7070/datamodelconf").then(function(res) {
           return res.data;
         }, function() {
           return "Unable to connect server";
@@ -23,7 +23,7 @@ angular.module('datamill')
       postDataModel: function(postdata) {
         return $http({
           method: "POST",
-          url: "http://localhost:7070/datamodel",
+          url: "http://172.23.238.188:7070/datamodel",
           data: postdata,
           'content-type': "aplication/json"
         }).then(function(res) {
