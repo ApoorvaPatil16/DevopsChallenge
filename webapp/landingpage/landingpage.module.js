00ff3bb13b9d5241a19606496639bb8fbaf3b81e
Prashant Farkya <prashant.farkya@gmail.com>
2016-08-30 19:25:07 +0530

    UI fixing

---
+++ webapp/landingpage/landingpage.module.js
@@ -0,0 +1,14 @@
+angular.module('datamill')
+  .config(['$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+      $stateProvider
+        .state('datamill.about', {
+          url: '/datamill',
+          views: {
+            "content@": {
+              templateUrl: "/landingpage/templates/landingpage.html"
+            }
+          }
+        });
+    }
+  ]);
