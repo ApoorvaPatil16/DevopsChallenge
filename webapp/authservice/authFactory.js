6483ea3a7c30a3efa2718c6007c54c709742193c
unknown <vishal221092@gmail.com>
2016-08-30 08:17:50 +0530

    sidenav changes

--- webapp/authservice/authFactory.js
+++ webapp/authservice/authFactory.js
@@ -2,7 +2,7 @@ angular.module('datamill')
   .factory('authFactory', function($http) {
     return {
       getNavbarItems: function() {
-        return $http.get("http://172.23.238.188:7070/navbarItems").then(function(res) {
+        return $http.get("http://localhost:7070/navbarItems").then(function(res) {
           return res.data;
         })
       },
