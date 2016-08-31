5684debacdc290175063592c48d756cb147edb93
Prashant Farkya <prashant.farkya@gmail.com>
2016-08-31 14:15:01 +0530

    json-server integrate with express

--- webapp/datamodeldefination/datafeed/services/datafeedservice.js
+++ webapp/datamodeldefination/datafeed/services/datafeedservice.js
@@ -2,7 +2,7 @@ angular.module('datamill')
   .factory('datafeedService', function($http, $log) {
     return {
       getTransportType: function() {
-        return $http.get('http://localhost:7070/transporttype')
+        return $http.get('api/transporttype')
           .then(function(response) {
             $log.info(response);
             return response.data;
--- webapp/datasource/services/listdataService.js
+++ webapp/datasource/services/listdataService.js
@@ -2,7 +2,7 @@ angular.module('datamill')
   .factory('listdataService', function($http, $log) {
     return {
       getdatasources: function() {
-        return $http.get('http://localhost:7070/datasource')
+        return $http.get('api/datasource')
           .then(function(response) {
             $log.info(response);
             return response.data;
