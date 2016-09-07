6812e94095af8d8ac86036873100b2d7ce2e36e9
Apoorva Patil <apoorvapatil92@gmail.com>
2016-09-07 16:03:14 +0530

    Fixing dashboard search function

--- webapp/dashboard/controllers/dashboardCtrl.js
+++ webapp/dashboard/controllers/dashboardCtrl.js
@@ -5,11 +5,33 @@ angular.module('datamill')
       $scope.datamodel = res;
     });
     console.log($scope.datamodel);
-    $scope.getSearchResults = function() {
-      $log.log("In simple search");
-      listDataModelsService.getDatamodelSearch($scope.search).then(function(res) {
+    /*  $scope.getSearchResults = function() {
+    $log.log("In simple search");
+    listDataModelsService.getDatamodelSearch($scope.search).then(function(res) {
+      $scope.datamodel = res;
+    });
+    $log.log($scope.datamodel);
+  }
+*/
+    $scope.searchAll = function() {
+      $scope.count = 0;
+      $scope.status = true;
+      listDataModelsService.getDatamodelsList().then(function(res) {
         $scope.datamodel = res;
-      });
-      $log.log($scope.datamodel);
+      })
+    }
+
+    $scope.searchDataFunction = function() {
+      $scope.count = 0;
+      $scope.status = true;
+      listDataModelsService.getDatamodelSearch($scope.searchData).then(function(res) {
+        $scope.datamodel = res;
+      })
+    }
+    $scope.searchInputFunction = function() {
+      $scope.searchData = "";
+      $scope.closeStyle = {
+        'background': 'rgb(221,240,221)'
+      }
     }
   }])
