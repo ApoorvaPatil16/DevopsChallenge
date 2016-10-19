d07269bf1f823d32fc7ee066722b1e831ab6a638
lokesh123456 <lokeshvicky576@gmail.com>
2016-10-19 18:43:46 +0530

    appconf

--- appconf.js
+++ appconf.js
@@ -1,13 +1,13 @@
 module.exports = {
-  MONGO_HOST: process.env.MONGO_HOST || "mongodb://localhost",
-  MONGO_PORT: process.env.MONGO_PORT || 27017,
-  MONGO_DB: "datamill_demo_1",
-  REDIS_HOST: process.env.REDIS_HOST || "localhost",
-  REDIS_POST: process.env.REDIS_POST || 6379,
-  GOOGLE_CLIENTID: "440591203673-vlfttsppabo6ldkdleu08spg4jc7hmvm.apps.googleusercontent.com",
-  GOOGLE_SECRET: "Kh_5Yf0pS0ub30e16u4_nB49",
-  GOOGLE_URI: "http://localhost:8080",
-  GITHUB_CLIENTID: "15ccef8b737c4839249e",
-  GITHUB_SECRET: "196e0ee75fe8854c6f687712d6021a7fb0e01016",
+    MONGO_HOST: process.env.MONGO_HOST || "mongodb://localhost",
+    MONGO_PORT: process.env.MONGO_PORT || 27017,
+    MONGO_DB: "datamill_demo_1",
+    REDIS_HOST: process.env.REDIS_HOST || "localhost",
+    REDIS_POST: process.env.REDIS_POST || 6379,
+    GOOGLE_CLIENTID: "440591203673-vlfttsppabo6ldkdleu08spg4jc7hmvm.apps.googleusercontent.com",
+    GOOGLE_SECRET: "Kh_5Yf0pS0ub30e16u4_nB49",
+    GOOGLE_URI: "http://localhost:8080",
+    GITHUB_CLIENTID: "15ccef8b737c4839249e",
+    GITHUB_SECRET: "196e0ee75fe8854c6f687712d6021a7fb0e01016",
 
 }
