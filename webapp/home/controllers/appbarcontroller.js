753f72f89902d3e68dd4960df4c42012b7dac902
lokesh123456 <lokeshvicky576@gmail.com>
2016-10-19 18:28:02 +0530

    Second commit

--- webapp/home/controllers/appbarcontroller.js
+++ webapp/home/controllers/appbarcontroller.js
@@ -1,48 +1,48 @@
 angular.module('datamill')
-  .controller('appBarCtrl', ['$scope', '$mdSidenav', '$mdPanel', 'authFactory', '$state',
-    function($scope, $mdSidenav, $mdPanel, authFactory, $state) {
-      $this = this;
-      // Fetch data from json server with call to factory service which in turn makes a http request
-      authFactory.getNavbarItems().then(function(res) {
-        $scope.appName = res[0].appName[0].name;
-        $scope.sideNavItems = res[0].sideNavItems;
-        $scope.accountItems = res[0].accountItems;
-        $scope.materialIcons = res[0].materialIcons;
-        $scope.sideNavBottomItems = res[0].sideNavBottomItems;
-      });
+    .controller('appBarCtrl', ['$scope', '$mdSidenav', '$mdPanel', 'authFactory', '$state',
+        function($scope, $mdSidenav, $mdPanel, authFactory, $state) {
+            $this = this;
+            // Fetch data from json server with call to factory service which in turn makes a http request
+            authFactory.getNavbarItems().then(function(res) {
+                $scope.appName = res[0].appName[0].name;
+                $scope.sideNavItems = res[0].sideNavItems;
+                $scope.accountItems = res[0].accountItems;
+                $scope.materialIcons = res[0].materialIcons;
+                $scope.sideNavBottomItems = res[0].sideNavBottomItems;
+            });
 
-      $scope.toggleLeft = buildToggler;
-      $scope.profile = function() {
-        $state.go("datamill.userprofile");
-      }
-      $scope.goHome = function() {
-        $state.go('datamill');
-      }
+            $scope.toggleLeft = buildToggler;
+            $scope.profile = function() {
+                $state.go("datamill.userprofile");
+            }
+            $scope.goHome = function() {
+                $state.go('datamill.dashboard');
+            }
 
-      function buildToggler(navID) {
-        $mdSidenav(navID).toggle().then(function() {});
-      }
-      $scope.showNotification = function() {
-        var position = $mdPanel.newPanelPosition()
-          .absolute()
-          .right()
-          .top("60px");
+            function buildToggler(navID) {
+                $mdSidenav(navID).toggle().then(function() {});
+            }
+            $scope.showNotification = function() {
+                var position = $mdPanel.newPanelPosition()
+                    .absolute()
+                    .right()
+                    .top("60px");
 
-        var config = {
-          animation: undefined,
-          attachTo: angular.element(document.body),
-          controller: notificationDialogCtrl,
-          controllerAs: 'notifyCtrl',
-          templateUrl: '/notifications/templates/notifications.html',
-          panelClass: 'notifications-content',
-          position: position,
-          trapFocus: true,
-          zIndex: 150,
-          clickOutsideToClose: true,
-          clickEscapeToClose: true,
-        };
+                var config = {
+                    animation: undefined,
+                    attachTo: angular.element(document.body),
+                    controller: notificationDialogCtrl,
+                    controllerAs: 'notifyCtrl',
+                    templateUrl: '/notifications/templates/notifications.html',
+                    panelClass: 'notifications-content',
+                    position: position,
+                    trapFocus: true,
+                    zIndex: 150,
+                    clickOutsideToClose: true,
+                    clickEscapeToClose: true,
+                };
 
-        $mdPanel.open(config);
-      }
-    }
-  ]);
+                $mdPanel.open(config);
+            }
+        }
+    ]);
