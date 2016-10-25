69946083f60758d6903ca0313b08381420a03d7c
lokesh123456 <lokeshvicky576@gmail.com>
2016-10-25 10:50:58 +0530

    datamillModelChanges

--- generatorapp/pipelining/passdatasource.js
+++ generatorapp/pipelining/passdatasource.js
@@ -4,24 +4,25 @@ var importsourceModel = require('../../datamillserver/datasource/importdataSchem
 var client = redis.createClient(appconf.REDIS_PORT, appconf.REDIS_HOST);
 
 function passdatasource(sourcename, email, balancer) {
-  //console.log("inside passdatasource");
-  importsourceModel.find({ sourcename: sourcename, email: email }, function(err, docs) {
-    if (err) {
-      console.log("error in passdatasource in fetching data from datasource");
-      console.log(err);
-    } else {
-      //console.log("success callback from passdatasource");
-      var data = docs[0].data;
-      for (i = 0; i < balancer; i++) {
-        var randomvalue = Math.floor(Math.random() * (data.length));
-        var keys = Object.keys(data[0]);
-        if (data[randomvalue][keys[0]])
-          client.lpush(sourcename + "_" + email, data[randomvalue][keys[0]]);
-      }
-    }
-  });
+    //console.log("inside passdatasource");
+    console.log("source name is ", sourcename);
+    importsourceModel.find({ sourcename: sourcename, email: email }, function(err, docs) {
+        if (err) {
+            console.log("error in passdatasource in fetching data from datasource");
+            console.log(err);
+        } else {
+            //console.log("success callback from passdatasource");
+            var data = docs[0].data;
+            for (i = 0; i < balancer; i++) {
+                var randomvalue = Math.floor(Math.random() * (data.length));
+                var keys = Object.keys(data[0]);
+                if (data[randomvalue][keys[0]])
+                    client.lpush(sourcename + "_" + email, data[randomvalue][keys[0]]);
+            }
+        }
+    });
 }
 module.exports = {
-  passdatasource: passdatasource,
-  client: client
+    passdatasource: passdatasource,
+    client: client
 };
--- generatorapp/pipelining/redisSubscribe.js
+++ generatorapp/pipelining/redisSubscribe.js
@@ -1,12 +1,13 @@
 var redisClient = require('./passdatasource');
 
 function getRealData(sourcename, email, cb) {
-  // console.log(redisClient.client.lpop());	
-  // console.log(client.lrange("First names",0,0));
-  redisClient.client.lpop(sourcename + "_" + email, function(err, data) {
-    cb(data);
-  });
+    // console.log(redisClient.client.lpop());	
+    // console.log(client.lrange("First names",0,0));
+    console.log("inside redis subscribe");
+    redisClient.client.lpop(sourcename + "_" + email, function(err, data) {
+        cb(data);
+    });
 }
 module.exports = {
-  getRealData: getRealData
+    getRealData: getRealData
 };
